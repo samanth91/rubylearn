883a575717c233fb506a64a732df7c322b2646ab
samanth91 <samnagul@umail.iu.edu>
2016-06-27 15:24:00 -0400

    adding necessary changes

--- millionsold.rb
+++ millionsold.rb
@@ -1,3 +1,3 @@
 puts "seconds if 1246000000 old"
-millionseconds = 1246000000/60/60/34/365
-puts millionseconds
+million_seconds = 1246000000 / 60 / 60 / 34 / 365
+puts million_seconds
--- numberofhours.rb
+++ numberofhours.rb
@@ -1,3 +1,3 @@
 puts "calculate number of hours"
-hours = 26 * 8750 * 60 *60
+hours = 365 * 24
 puts hours

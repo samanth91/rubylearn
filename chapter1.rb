e31aa79d0194998c059821dfe43eee0d36b3c5e0
samanth91 <samnagul@umail.iu.edu>
2016-06-29 12:29:21 -0400

    adding chapter1 changes

--- chapter1.rb
+++ chapter1.rb
@@ -1,36 +1,36 @@
-# calculator example
-
+# Calculator example
 puts 1 + 2
 puts 2 * 3
 puts 5 - 8
 puts 9 / 2
 
-# Calculate years if 1246 million seconds old 
+# Calculate number of hours in a year
+puts "calculate number of hours"
+hours = 365 * 24
+puts hours
 
+# Calculate minutes in decade 
+puts "number of minutes in a decade"
+minutes = 10 * hours * 60
+puts minutes
+
+# Calculate number of seconds in a year
+puts "calculate number of seconds in a year"
+seconds = hours * 60 * 60
+puts seconds
+
+# Calculate number of seconds old
+puts "number of seconds old"
+seconds_old = (25 * hours) + (103 * 24 * 60  * 60) + (22 * 60 * 60) + (32 * 60)
+puts seconds_old
+
+# Calculate number of choclates in a life time
+puts "calculate the number of choclates in a life time"
+choclates = 365 * 2 * 70
+puts choclates
+
+# Calculate years if 1246 million seconds old 
 puts "seconds if 1246000000 old"
 million_seconds = 1246000000 / 60 / 60 / 34 / 365
 puts million_seconds
 
-# Calculate minutes in decade 
-
-puts "number of minutes in a decade"
-minutes = 10 * 365 * 24 * 60
-puts minutes
-
-# Calculate number of hours in a year
-
-puts "calculate number of hours"
-hours = 365 * 24
-puts hours  
-
-# Calculate number of seconds old
-
-puts "number of seconds old"
-seconds_old = (25 * 365 * 24) + (103 * 24 * 60  * 60) + (22 * 60 * 60) + (32 * 60)
-puts seconds_old
-
-# Calculate number of seconds in a year
-
-puts "calculate number of seconds in a year"
-seconds = 365 * 24 * 60 * 60
-puts seconds

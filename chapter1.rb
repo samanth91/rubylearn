d640efe33f57925eb26504916fab6652ced0ebf9
samanth91 <samnagul@umail.iu.edu>
2016-06-29 17:12:24 -0400

    adding spelling and comment changes

--- chapter1.rb
+++ chapter1.rb
@@ -5,32 +5,32 @@ puts 5 - 8
 puts 9 / 2
 
 # Calculate number of hours in a year
-puts "calculate number of hours"
-hours = 365 * 24
-puts hours
+puts "number of hours in year"
+hours_in_year = 365 * 24
+puts hours_in_year
 
 # Calculate minutes in decade 
 puts "number of minutes in a decade"
-minutes = 10 * hours * 60
+minutes = 10 * hours_in_year * 60
 puts minutes
 
 # Calculate number of seconds in a year
 puts "calculate number of seconds in a year"
-seconds = hours * 60 * 60
+seconds = hours_in_year * 60 * 60
 puts seconds
 
 # Calculate number of seconds old
 puts "number of seconds old"
-seconds_old = (25 * hours) + (103 * 24 * 60  * 60) + (22 * 60 * 60) + (32 * 60)
+seconds_old = (25 * hours_in_year) + (103 * 24 * 60  * 60) + (22 * 60 * 60) + (32 * 60)
 puts seconds_old
 
-# Calculate number of choclates in a life time
+# Calculate number of chocolates in a life time
 puts "calculate the number of choclates in a life time"
-choclates = 365 * 2 * 70
-puts choclates
+chocolates = 365 * 2 * 70 # Average of 70 years and 2 chocolates per day
+puts chocolates
 
-# Calculate years if 1246 million seconds old 
-puts "seconds if 1246000000 old"
-million_seconds = 1246000000 / 60 / 60 / 34 / 365
+# Calculate age if 1246 million seconds old 
+puts "age if 1246000000 seconds old"
+million_seconds = 1_246_000_000 / 60 / 60 / 34 / 365
 puts million_seconds
 

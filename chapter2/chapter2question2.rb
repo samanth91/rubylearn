9717ce9384f166461d741f804431c30c3215a079
samanth91 <samnagul@umail.iu.edu>
2016-06-30 22:32:57 -0400

    adding changes

--- chapter2/chapter2question2.rb
+++ chapter2/chapter2question2.rb
@@ -3,6 +3,9 @@
 puts "Enter your favourite number"
 favourite_number = gets.chomp
 favourite_number_better = favourite_number.to_f + 1
+
 puts 'Your favourite number ' + favourite_number
 puts 'The biggger and better number is ' + favourite_number_better.to_s 
 
+puts "Your favourite number is #{favourite_number.to_s}"
+puts "The biggest and better number is #{favourite_number.to_i + 1}"

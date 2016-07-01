83d9b750535e8ba09638bf432f97e2f748a2d0af
samanth91 <samnagul@umail.iu.edu>
2016-07-01 13:57:12 -0400

    adding chapter 5 files

---
+++ chapter5/chapter5_exercise1.rb
@@ -0,0 +1,11 @@
+# Reply of a angry boss 
+puts "Angry Boss Reply".center(50)
+puts ''
+
+puts "what do you want"
+want = gets.chomp
+
+# Printing the boss reply with concatination
+puts 'WHADDYA MEAN "I WANT ' + want.upcase + "." + '"?!? YOU\'RE FIRED!!'
+# Printing the boss reply with interpolation
+puts "WHADDYA MEAN \"I WANT #{want.upcase}.\"?!? YOU'RE FIRED!!"
---
+++ chapter5/chapter5_exercise2.rb
@@ -0,0 +1,10 @@
+# Display the table of contents using dynamically entered page width
+
+page_width = 50
+
+puts 'Table of Contents'.center(page_width)
+puts ''
+
+puts 'Chapter 1: Numbers'.ljust(page_width / 2) + 'page 1'.rjust(page_width / 2)
+puts 'Chapter 2: Letters'.ljust(page_width / 2) + 'page 72'.rjust(page_width / 2)
+puts 'Chapter 3: Variables'.ljust(page_width / 2) + 'page 118'.rjust(page_width / 2)

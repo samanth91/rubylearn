2d1055ba971376a9a3fda194e91e3996fc4bc92b
samanth91 <samnagul@umail.iu.edu>
2016-07-01 11:53:43 -0400

    adding the changes

--- chapter2/chapter2_exercise2.rb
+++ chapter2/chapter2_exercise2.rb
@@ -6,7 +6,7 @@ better_than_favourite_number = favourite_number.to_i + 1
 
 # Printing the results using two variables
 puts 'Your favourite number is ' + favourite_number
-puts 'The biggger and better number is ' + better_than_favourite_number.to_s 
+puts 'The bigger and better number is ' + better_than_favourite_number.to_s 
 
 # Printing results using favourite_number variable
 puts "Your favourite number is #{favourite_number.to_s}"

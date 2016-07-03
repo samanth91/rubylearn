11281b132f5fb9f08e66b271cfa8851693e72757
samanth91 <samnagul@umail.iu.edu>
2016-07-02 22:52:18 -0400

    adding changes

---
+++ chapter6/jatakam.rb
@@ -0,0 +1,10 @@
+puts "i am a fortune teller"
+puts "tell me your name"
+name = gets.chomp
+if name == 'samanth'
+  puts 'get the hell out of here'
+else
+  puts 'you can stay'
+  puts 'not as worst as samanth'
+end
+ 

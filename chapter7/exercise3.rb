9b0a0d6f2e100709e30c90a3c2a38d4f8245a638
samanth91 <samnagul@umail.iu.edu>
2016-07-06 09:35:46 -0400

    Adding changes to exercise 3

--- chapter7/exercise3.rb
+++ chapter7/exercise3.rb
@@ -1,12 +1,11 @@
 # Table of contents
 
 table_of_contents = ['Table of Contents', 'Chapter 1: Getting Started','page 1', 'Chapter 2: Numbers','page 72', 'Chapter 3: Letters',' page 118']
-index_count = table_of_contents.count
 page_width = 60
+content_count = table_of_contents.count
 i = 0
-
 puts "#{table_of_contents[0].center(page_width)}\n\n"
 
-until i == 6
-puts "#{table_of_contents[i += 1].ljust(page_width / 2)}  #{table_of_contents[i += 1].rjust(page_width / 2)}"
+until i == content_count - 1
+  puts "#{table_of_contents[i += 1].ljust(page_width / 2)}  #{table_of_contents[i += 1].rjust(page_width / 2)}"
 end

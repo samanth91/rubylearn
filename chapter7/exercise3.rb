ffa24a5a50a283487327c64e9bde8ef2a66dbd1c
samanth91 <samnagul@umail.iu.edu>
2016-07-06 10:03:07 -0400

    adding changes to ex3

--- chapter7/exercise3.rb
+++ chapter7/exercise3.rb
@@ -2,10 +2,9 @@
 
 table_of_contents = ['Table of Contents', 'Chapter 1: Getting Started','page 1', 'Chapter 2: Numbers','page 72', 'Chapter 3: Letters',' page 118']
 page_width = 60
-content_count = table_of_contents.count
 i = 0
 puts "#{table_of_contents[0].center(page_width)}\n\n"
 
-until i == content_count - 1
+until i == table_of_contents.count - 1
   puts "#{table_of_contents[i += 1].ljust(page_width / 2)}  #{table_of_contents[i += 1].rjust(page_width / 2)}"
 end

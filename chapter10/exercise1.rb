98a65d5743f4dae4375b44f40a4d45701a2fc752
samanth91 <samnagul@umail.iu.edu>
2016-07-12 14:58:55 -0400

    adding changes

--- chapter10/exercise1.rb
+++ chapter10/exercise1.rb
@@ -1,18 +1,21 @@
-# Grand father clock
-
-def grand_father_clock(&block)
-   current_hour = Time.now.hour
+def grand_father_clock(some_proc)
+   current_hour = Time.new.hour
    
    if current_hour == 0
-     current_hour = 12
-   elsif 
-     current_hour >= 13
-     current_hour -= 12
+     current_hour += 12
+   elsif current_hour > 12
+     current_hour -=12
    end
-  
+
    current_hour.to_i.times do
-     block.call
+     some_proc.call
    end
 end
 
-grand_father_clock {puts "DONG!"}
+dong_proc = Proc.new do
+  puts 'DONG!'
+end
+
+grand_father_clock dong_proc
+
+ 

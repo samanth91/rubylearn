57aee5f716483e9ec03bb75c9ad75ea2ffd598e6
samanth91 <samnagul@umail.iu.edu>
2016-07-15 16:28:21 -0400

    adding space changes

--- chapter10/exercise3.rb
+++ chapter10/exercise3.rb
@@ -2,25 +2,25 @@ $nesting_depth = 0
 $space = ' '
 
 def log(block_description, &block)
-   puts $space * $nesting_depth + 'Beginning "' + block_description + '" ... '
-   $nesting_depth += 1
-   value_returned = block.call
-   $nesting_depth -= 1
-   print $space * $nesting_depth + '... "'+ block_description + '" finished, returning: '
-   puts $space * $nesting_depth + value_returned.to_s
+  puts $space * $nesting_depth + 'Beginning "' + block_description + '" ... '
+  $nesting_depth += 1
+  value_returned = block.call
+  $nesting_depth -= 1
+  print $space * $nesting_depth + '... "'+ block_description + '" finished, returning: '
+  puts $space * $nesting_depth + value_returned.to_s
 end
 
 log 'outer block' do
-   log 'some little block' do
-      log 'teeny-tiny block' do
-         'lots of love'
-      end
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      'lots of love'
+    end
 
        42
-   end
+  end
 
    log 'yet another block' do
-      'I Love Indian food!'
+     'I Love Indian food!'
    end
   
    true
